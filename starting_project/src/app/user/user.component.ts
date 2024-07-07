da8fbee229915fb8a63dd8158bd09cfb86635aaf
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-07 06:24:16 +0300

    update

--- starting_project/src/app/user/user.component.ts
+++ starting_project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIned = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,5 +8,35 @@ const randomIned = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  slectedUser = DUMMY_USERS[randomIned];
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  // Getter
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {}
 }
+
+// const randomIned = Math.floor(Math.random() * DUMMY_USERS.length);
+// // First way to work with state
+// slectedUser = DUMMY_USERS[randomIned];
+// onSelectUser() {
+//   const randomIned = Math.floor(Math.random() * DUMMY_USERS.length);
+//   this.slectedUser = DUMMY_USERS[randomIned];
+// }
+// // Getter
+// get imagePath() {
+//   return 'assets/users/' + this.slectedUser.avatar;
+// }
+
+// // Second way to work with state {{ Signals }}
+// slectedUser = signal(DUMMY_USERS[randomIned]);
+// onSelectUser() {
+//   const randomIned = Math.floor(Math.random() * DUMMY_USERS.length);
+//   this.slectedUser.set(DUMMY_USERS[randomIned]);
+// }
+// imagePath = computed(() => 'assets/users/' + this.slectedUser().avatar);
+// avatar = input<string>(');
+// avatar = input.required<string>();
