c631e61f8f81b2233b0a59ab6bbff5f5d474f813
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-08 17:25:29 +0300

    update

--- project_2/src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
+++ project_2/src/app/dashbord/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControllComponent } from '../../../shared/controll/controll.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
+  imports: [ButtonComponent, ControllComponent],
 })
 export class NewTicketComponent {}
--- project_2/src/app/shared/controll/controll.component.ts
+++ project_2/src/app/shared/controll/controll.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-controll',
   standalone: true,
   imports: [],
   templateUrl: './controll.component.html',
-  styleUrl: './controll.component.css'
+  styleUrl: './controll.component.css',
 })
 export class ControllComponent {
-
+  label = input.required<string>();
 }
