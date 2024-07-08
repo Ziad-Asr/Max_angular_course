e882a95fbec2af224dfea1a85124b0e36b20719d
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-08 17:00:45 +0300

    update

---
+++ project_2/src/app/shared/controll/controll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controll',
+  standalone: true,
+  imports: [],
+  templateUrl: './controll.component.html',
+  styleUrl: './controll.component.css'
+})
+export class ControllComponent {
+
+}
