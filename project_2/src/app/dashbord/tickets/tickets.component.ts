120fbb8c1aa61aa7d0c411513a9cc64fa3d52f2f
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-08 16:12:44 +0300

    update

--- project_2/src/app/dashbord/tickets/tickets.component.ts
+++ project_2/src/app/dashbord/tickets/tickets.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
+  imports: [TicketComponent],
 })
-export class TicketsComponent {
-
-}
+export class TicketsComponent {}
