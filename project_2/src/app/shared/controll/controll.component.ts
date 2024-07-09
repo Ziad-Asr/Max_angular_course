d788a1fe0729a760c60ebb42982535ad07c4fd0d
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-09 03:21:34 +0300

    update

--- project_2/src/app/shared/controll/controll.component.ts
+++ project_2/src/app/shared/controll/controll.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-controll',
@@ -9,8 +17,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None, // To pass styles when using the (ng-content) {not get encasulated here only}
   host: {
     class: 'controll',
+    '(click)': 'onClick()',
   }, // Use to put style of the class (control) in a wrapper div around theis reusable componenet.
 })
 export class ControllComponent {
+  // @HostBinding('class') className = 'controll'; // Insted of (host)
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+
+  private element = inject(ElementRef); // Get access to the component host html
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.element);
+  }
+
   label = input.required<string>();
 }
