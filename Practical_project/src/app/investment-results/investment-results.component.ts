4dcb630a256baa56b2af8f344eb721ac1279864e
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-30 12:55:45 +0300

    update

--- Practical_project/src/app/investment-results/investment-results.component.ts
+++ Practical_project/src/app/investment-results/investment-results.component.ts
@@ -1,14 +1,15 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() result?: {
+  @Input() results?: {
     year: number;
     interest: number;
     valueEndOfYear: number;
