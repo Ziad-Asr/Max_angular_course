8fe71c2d8020b0933c7cb957fc9ceba55668af54
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-30 10:35:03 +0300

    update

--- Routing_Project/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
-
-export const routes: Routes = [
-  {
-    path: '/tasks',
-    component: TasksComponent,
-  },
-];
