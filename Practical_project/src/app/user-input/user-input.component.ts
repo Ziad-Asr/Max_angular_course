201690093b6cc74547344a04c5c22a8aa5267a4f
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-30 12:10:17 +0300

    update

--- Practical_project/src/app/user-input/user-input.component.ts
+++ Practical_project/src/app/user-input/user-input.component.ts
@@ -1,29 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css',
+  styleUrls: ['./user-input.component.css'],
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<{
-    initialInvestment: number;
-    duration: number;
-    expectedReturn: number;
-    annualInvestment: number;
-  }>();
+  @Output() calculate = new EventEmitter<InvestmentInput>();
 
-  enteredIntialInvestment = '0';
+  enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.enteredIntialInvestment,
+      initialInvestment: +this.enteredInitialInvestment,
       annualInvestment: +this.enteredAnnualInvestment,
       expectedReturn: +this.enteredExpectedReturn,
       duration: +this.enteredDuration,
