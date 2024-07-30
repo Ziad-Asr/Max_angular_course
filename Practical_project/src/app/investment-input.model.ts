c71a0b272578480c4fd3868db9cc369fc02e82fa
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-07-30 12:39:28 +0300

    update

--- Practical_project/src/app/investment-input.model.ts
+++ Practical_project/src/app/investment-input.model.ts
@@ -4,3 +4,9 @@ export interface InvestmentInput {
   expectedReturn: number;
   annualInvestment: number;
 }
+
+// import type => Make sure that the import is a ( type or interface )
+
+// ? , !
+// ? => {{ Might }} have a value of that shape
+// ! => Will {{ always }} have a value of that shape
---
+++ Practical_project/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  @Input() result?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+}
+
+// import type => Make sure that the import is a ( type or interface )
+
+// ? , !
+// ? => {{ Might }} have a value of that shape
+// ! => Will {{ always }} have a value of that shape
