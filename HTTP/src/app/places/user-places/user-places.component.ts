f428ec70c80064b108d82d82225fc7ea3ddeb51e
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-08-01 09:20:35 +0300

    update

--- HTTP/src/app/places/user-places/user-places.component.ts
+++ HTTP/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +12,47 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal<boolean>(false);
+  error = signal<string>('');
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          this.error.set('Something went wrong fetching your favorite places');
+          this.isFetching.set(false);
+          return throwError(
+            () =>
+              new Error('Something went wrong fetching your favorite places')
+          );
+        })
+      )
+      .subscribe({
+        next: (resData) => {
+          this.places.set(resData);
+          this.isFetching.set(false);
+        },
+        error: (err: Error) => {
+          this.error.set(err.message);
+          this.isFetching.set(false);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
