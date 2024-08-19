601122e01e8dd1f2dc2e329b62a915d094041d53
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-08-19 22:11:37 +0300

    update

---
+++ NGRX/src/app/store/counter.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
+
+const intialState = 0;
+
+export const counterReducer = createReducer(
+  intialState,
+  on(increment, (state, action) => state + action.value)
+);
+
+// export function counterReducer(state = intialState, action: any) {
+//   if (action.type === '[Counter] Increment') {
+//     return state + action.value;
+//   }
+//   return state;
+// }
--- NGRX/src/main.ts
+++ NGRX/src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
+import { counterReducer } from './app/store/counter.reducer';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore()]
+  providers: [provideStore({ counter: counterReducer })],
 });
+
+// When putting ($) in the end of a variable => this indicates that that variable is holdingan ((objservable))  {Not Mandatory}.
+// { | async} pipe => is used to outomatically update the value when changing on the store.
+// {{[Counter] Increment'}} in (store => couter.actions.ts) => We added [Counter] here to ensure that this unique indentifier is not repeated.
